e6f669daa4caf96a693da5a9fc7273565584bcda
magikarp13 <leonardcoutinho@gmail.com>
2026-08-09 02:52:30 -0300

    chore: update cask to v1.0.6

--- Casks/simpletranscribe.rb
+++ Casks/simpletranscribe.rb
@@ -1,6 +1,6 @@
 cask "simpletranscribe" do
-  version "1.0.5"
-  sha256 "5f4f2065a64a7d0f525cf5974f81809606250040a87e45e11efe74890b6606f1"
+  version "1.0.6"
+  sha256 "97ebd53faa9948f2e92e6a3c0ea234d3195164ef31d9762f87f1db5e78a9b90e"
 
   url "https://github.com/redmagikarp13/simpletranscribe/releases/download/v#{version}/SimpleTranscribe-Mac.zip"
   name "Simple Transcribe"
