187a0f90a3bded2839fce9d65b32667ce9ce3d63
Leonardo Coutinho <leonardcoutinho@gmail.com>
2026-08-09 03:21:31 -0300

    Update Simple Transcribe to version 1.0.8

--- Casks/simpletranscribe.rb
+++ Casks/simpletranscribe.rb
@@ -1,6 +1,6 @@
 cask "simpletranscribe" do
-  version "1.0.7"
-  sha256 "c4e149f281ec1ed75fba4b7966f830d8e34e2ec3acb8856e196cf4ca5f7b2fc6"
+  version "1.0.8"
+  sha256 "2b54e4b80c40c17433cc416c75f4a0279cc6249ea688f71b80d9e21a0d032eb2"
 
   url "https://github.com/redmagikarp13/simpletranscribe/releases/download/v#{version}/SimpleTranscribe-Mac.zip"
   name "Simple Transcribe"
